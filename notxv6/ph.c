8a9f90b4710f0b048a7f1523c1f145acec73119b
Cy <925206795@qq.com>
2025-05-23 14:35:20 +0800

    thread

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -7,7 +7,7 @@
 
 #define NBUCKET 5
 #define NKEYS 100000
-
+pthread_mutex_t lock[NBUCKET];
 struct entry {
   int key;
   int value;
@@ -40,6 +40,7 @@ void put(int key, int value)
 {
   int i = key % NBUCKET;
 
+  pthread_mutex_lock(&lock[i]);
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
@@ -53,19 +54,20 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+  pthread_mutex_unlock(&lock[i]);
 }
 
 static struct entry*
 get(int key)
 {
   int i = key % NBUCKET;
-
+  pthread_mutex_lock(&lock[i]);
 
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
-
+  pthread_mutex_unlock(&lock[i]);
   return e;
 }
 
@@ -102,7 +104,6 @@ main(int argc, char *argv[])
   pthread_t *tha;
   void *value;
   double t1, t0;
-
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
     exit(-1);
@@ -114,7 +115,10 @@ main(int argc, char *argv[])
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
   }
-
+  for(int i = 0;i < NBUCKET;++i)
+  {
+    pthread_mutex_init(&lock[i],NULL);
+  }
   //
   // first the puts
   //
